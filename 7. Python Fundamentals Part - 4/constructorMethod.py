0f2db47c255e43aeaa270d37d93769b142d87545
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-11-24 19:45:00 +0530

    Object Oriented Programming: Constructor - init() Method.

--- 7. Python Fundamentals Part - 4/constructorMethod.py
+++ 7. Python Fundamentals Part - 4/constructorMethod.py
@@ -2,16 +2,23 @@
 
 # Class
 class student:
-    def __init__(self, name, year, course, roll_no):
+    def __init__(self, name, year, course, roll_no, cgpa):
         print("A Constructor was called!")
         self.name = name
         self.year = year
         self.course = course
         self.roll_no = roll_no
+        self.cgpa = cgpa
+        
+    def getCGPA(self):
+        return self.cgpa
 
 # Object
-stu1 = student("Satinder Singh Sall", 1, "MCA", 180)
-stu2 = student("Soni Vaibhav Kumar", 1, "MCA", 207)
+stu1 = student("Satinder Singh Sall", 1, "MCA", 180, 8.5)
+stu2 = student("Soni Vaibhav Kumar", 1, "MCA", 207, 9.9)
 
 print(stu1.name, stu1.year, stu1.course, stu1.roll_no)
 print(stu2.name, stu2.year, stu2.course, stu2.roll_no)
+
+print(f"CGPA of {stu1.name} is {stu1.getCGPA()}")
+print(f"CGPA of {stu2.name} is {stu2.getCGPA()}")
