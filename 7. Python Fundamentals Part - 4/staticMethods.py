340e0e1b7f1d8f50979b345bde9900f99d7edf4a
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-11-25 23:09:07 +0530

    AI / ML: Methods in Python - Instance, Class Method & Static Methods.

---
+++ 7. Python Fundamentals Part - 4/staticMethods.py
@@ -0,0 +1,22 @@
+# Methods in Python - Instance, Class Method & Static Methods
+
+class laptop:
+    storage_type = "SSD"
+
+    def __init__(self, RAM, storage):
+        self.RAM = RAM
+        self.storage = storage
+
+    @classmethod
+    def get_storage_type(cls):
+        print(f"Storage type: {cls.storage_type}")
+
+    def get_info(self):
+        print(f"Laptop has {self.RAM} RAM & {self.storage} {self.storage_type}")
+
+lap1 = laptop("16GB", "152GB")
+lap2 = laptop("8GB", "252GB")
+
+lap1.get_info()
+
+lap1.get_storage_type()
