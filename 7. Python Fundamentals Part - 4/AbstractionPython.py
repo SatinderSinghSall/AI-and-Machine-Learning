2034863c14be79326d2d1d602ef46da345ac43f4
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-11-28 13:50:12 +0530

    AI / ML: OOP in Python: Abstraction Python.

--- 7. Python Fundamentals Part - 4/AbstractionPython.py
+++ 7. Python Fundamentals Part - 4/AbstractionPython.py
@@ -1,1 +1,23 @@
 # OOP in Python: Abstraction Python
+
+from abc import ABC, abstractmethod
+
+class Payment(ABC):                     # Abstract Class
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+class UPI(Payment):
+    def pay(self, amount):
+        print(f"Paid ₹{amount} through Google Pay.")
+
+class Card(Payment):
+    def pay(self, amount):
+        print(f"Paid ₹{amount} using Debit/Credit Card.")
+
+
+p1 = UPI()
+p2 = Card()
+
+p1.pay(250)
+p2.pay(800)
