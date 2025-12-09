33707435f6b98d9c8d0c4e5bbb901896da65311a
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-12-09 23:47:19 +0530

    AI / ML: SQL - Used CREATE TABLE in Database.

--- 15. SQL - Part 1/SQL.sql
+++ 15. SQL - Part 1/SQL.sql
@@ -6,4 +6,23 @@ DROP DATABASE satinder_database_2;
 CREATE DATABASE satinder_college;
 USE satinder_college;
 
+CREATE TABLE Students(
+	stuRoll_no INT,
+    stuName VARCHAR(50),
+    stuAge INT
+);
 
+INSERT INTO Students
+VALUES
+(001, "Satinder Singh Sall", 22);
+
+INSERT INTO Students
+VALUES
+(002, "Soni Vaibhav Kumar", 19),
+(003, "Swayam Kehsarwani", 22),
+(004, "Harsha HM", 22),
+(005, "K Santosh Reddy", 22);
+
+SELECT * FROM Students;
+
+DROP TABLE Students;
