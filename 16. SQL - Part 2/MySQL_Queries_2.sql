f9c058596252cabb105dc7cf0340326f139cf4a8
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-12-14 00:34:30 +0530

    AI / ML: MySQL Database - JOINTS in SQL.

--- 16. SQL - Part 2/MySQL_Queries_2.sql
+++ 16. SQL - Part 2/MySQL_Queries_2.sql
@@ -32,3 +32,61 @@ SELECT * FROM Orders;
 
 -- JOINTS in SQL:
 
+-- INNER JOIN
+SELECT * FROM Customers C INNER JOIN Orders O ON C.customer_id = O.customer_id;
+
+-- LEFT & RIGHT JOIN
+SELECT * FROM Customers C LEFT JOIN Orders O ON C.customer_id = O.customer_id;
+SELECT * FROM Customers C RIGHT JOIN Orders O ON C.customer_id = O.customer_id;
+
+-- OUTER JOIN & CROSS JOIN
+SELECT *
+FROM Customers C
+LEFT JOIN Orders O
+ON C.customer_id = O.customer_id
+
+UNION
+
+SELECT *
+FROM Customers C
+RIGHT JOIN Orders O
+ON C.customer_id = O.customer_id;
+
+
+SELECT *
+FROM Customers
+CROSS JOIN Orders;
+
+-- SELF JOIN
+SELECT 
+    C1.name AS Customer1,
+    C2.name AS Customer2,
+    C1.city
+FROM Customers C1
+INNER JOIN Customers C2
+ON C1.city = C2.city
+AND C1.customer_id < C2.customer_id;
+
+SELECT 
+    C1.name,
+    C2.name,
+    C1.city,
+    C2.city
+FROM Customers C1
+JOIN Customers C2
+ON C1.city <> C2.city;
+
+-- Practice Problem:
+SELECT C.customer_id, C.name, C.city, NULL AS order_id, NULL AS amount
+FROM Customers C
+LEFT JOIN Orders O
+ON C.customer_id = O.customer_id
+WHERE O.customer_id IS NULL
+
+UNION
+
+SELECT NULL, NULL, NULL, O.order_id, O.amount
+FROM Customers C
+RIGHT JOIN Orders O
+ON C.customer_id = O.customer_id
+WHERE C.customer_id IS NULL;
