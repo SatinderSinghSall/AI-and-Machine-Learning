decd463d18a5d0a342a31b6136a2a1cf0752f594
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-03-21 18:00:44 +0530

    AI / ML: Reinforcement Learning - Implementing DQN Algorithm for Flappy Bird Game -- Implemented Greedy & Decay.

--- 48. Reinforcement Learning (Part 4)/agant.py
+++ 48. Reinforcement Learning (Part 4)/agant.py
@@ -7,6 +7,7 @@ import yaml
 import torch
 import torch.nn as nn
 import torch.optim as optim 
+import random
 
 if torch.backend.mps.is_available():
     device = "mps"
@@ -25,11 +26,14 @@ class Agent:
 
         self.alpha = params["alpha"]
         self.gamma = params["gamma"]
+
         self.epsilon_init = params["epsilon_init"]
         self.epsilon_min = params["epsilon_min"]
         self.epsilon_decay_rate = params["epsilon_decay_rate"]
+
         self.replay_memory_sizes = params["replay_memory_sizes"]
         self.mini_batch_size = params["mini_batch_size"]
+
         self.network_sync_rate = params["network_sync_rate"]
         self.reward_threshold = params["reward_threshold"]
         
@@ -46,20 +50,32 @@ class Agent:
 
         if is_training:
             memory = ReplayMemory(self.replay_memory_sizes)
+            epsilon = self.epsilon_init
 
         for episode in itertools.count():
             state, _ = env.reset()
+            state = torch.tensor(state, dtype=torch.float, device=device)
+
             episode_rewards = 0
             terminated = False
 
             obs, _ = env.reset()
             while not terminated:
-                # Next action:
-                # (feed the observation to your agent here)
-                action = env.action_space.sample()
+                if is_training and random.random() < epsilon:
+                    # Next action:
+                    # (feed the observation to your agent here)
+                    action = env.action_space.sample() # Explore
+                    action = torch.tensor(state, dtype = torch.long, device = device)
+                else:
+                    with torch.no_grad():
+                        action = policy_dqn(state.unsqueeze(dim=0)).squeeze().argmax() # Exploit
 
                 # Processing: terminated => done
                 next_state, reward, terminated, _, _ = env.step(action)
+
+                # Create Tensors:
+                reward = torch.tensor(reward, dtype=torch.float, device=device)
+                next_state = torch.tensor(next_state, dtype=torch.float, device=device)
 
                 if is_training:
                     memory.append(state, action, next_state, reward, terminated)
@@ -71,6 +87,9 @@ class Agent:
                 state = next_state
                 episode_rewards = episode_rewards + reward
 
-            print(f"Episode: {episode + 1} with Total Reward: {episode_rewards}")
-            # env.close() # Commented to manually stop the training.
+            print(f"Episode: {episode + 1} with Total Reward: {episode_rewards} & Epsilon: {epsilon}")
+            
+            # Epsilon Decay:
+            epsilon = max(epsilon * self.decay, self.epsilon_min)
 
+            # env.close() # Commented to manually stop the training.
