9dd0e23b6d9b337832a452d6f7b079d34f6cb483
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-04-01 20:54:05 +0530

    AI / ML: Text Summarizer App using HuggingFace Transformer - Deep Learning – GenAI - Transformers
    Implemented:
    - Fine-Tuning our Transformer Model:
      - Working with our Model.
    - Training Arguments:
    - Training our Model:
    - Model Training: Slowest Part of the Project
    - Saving & Loading our Model
    - Testing the Summarisation Logic: Model
    - Understanding Client - Server Architecture

--- 51. Text Summarizer [Minor Project]/Text Summarizer App/app.py
+++ 51. Text Summarizer [Minor Project]/Text Summarizer App/app.py
@@ -1,1 +1,13 @@
 # Fast API: Backend Development
+
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from transformers import T5ForConditionalGeneration, T5Tokenizer
+import torch
+import re 
+from fastapi.templating import Jinja2Templates # UI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI(title="Text Summarizer App", description="Text Summarization using T5", version="1.0")
+
