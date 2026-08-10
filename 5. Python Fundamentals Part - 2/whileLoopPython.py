46476ab62fe6180a37dbf0c7ac87ddc6af419334
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-08-10 15:05:01 +0530

    AI / ML: Python Fundamentals - Loops.

--- 5. Python Fundamentals Part - 2/whileLoopPython.py
+++ 5. Python Fundamentals Part - 2/whileLoopPython.py
@@ -6,3 +6,10 @@ while i <= 5:
     print(f"Hello, World: {i}")
     i = i + 1
 print(f"After loops end the i value is: {i}")
+
+j = 1
+while j <= 5:
+    print("Hello, World.")
+    j = j + 1
+    print(f"Number: {i}")
+print("The value of j after loops end is: {j}")
